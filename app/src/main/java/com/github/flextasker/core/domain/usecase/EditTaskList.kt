9fd794006c655fff9a17fe3940edf7073cc0eecc
ABurachevsky <alexander.burachevsky@firstlinesoftware.com>
2023-09-16 18:40:52 +0300

    implement list creation

---
+++ app/src/main/java/com/github/flextasker/core/domain/usecase/EditTaskList.kt
@@ -0,0 +1,14 @@
+package com.github.flextasker.core.domain.usecase
+
+import com.github.flextasker.core.domain.repository.TaskListRepository
+import com.github.flextasker.core.model.TaskListInfo
+import javax.inject.Inject
+
+class EditTaskList @Inject constructor(
+    private val taskListRepository: TaskListRepository
+) {
+
+    suspend operator fun invoke(list: TaskListInfo) {
+        taskListRepository.editTaskList(list)
+    }
+}
