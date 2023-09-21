289e1b1253b7e7744f5d996b709ad7f008a96925
ABurachevsky <alexander.burachevsky@firstlinesoftware.com>
2023-09-21 21:06:25 +0300

    make sign up screen

--- app/src/main/java/com/github/flextasker/feature/auth/signup/SignUpFragment.kt
+++ app/src/main/java/com/github/flextasker/feature/auth/signup/SignUpFragment.kt
@@ -3,6 +3,8 @@ package com.github.flextasker.feature.auth.signup
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -12,6 +14,7 @@ import com.github.flextasker.core.ui.container.ViewController
 import com.github.flextasker.core.ui.container.viewContainer
 import com.github.flextasker.core.ui.di.ViewModelFactory
 import com.github.flextasker.core.ui.ext.applicationAs
+import com.github.flextasker.core.ui.ext.collectOnStarted
 import com.github.flextasker.databinding.FragmentSignUpBinding
 import com.github.flextasker.feature.auth.AuthComponent
 import javax.inject.Inject
@@ -34,6 +37,32 @@ class SignUpFragment : Fragment(R.layout.fragment_sign_up), ViewController<SignU
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.toolbar.setNavigationOnClickListener {
             findNavController().navigateUp()
+        }
+
+        binding.emailEditText.addTextChangedListener {
+            viewModel.email = it.toString()
+            viewModel.clearErrors()
+        }
+
+        binding.passwordEditText.addTextChangedListener {
+            viewModel.password = it.toString()
+            viewModel.clearErrors()
+        }
+
+        binding.confirmPasswordEditText.addTextChangedListener {
+            viewModel.confirmPassword = it.toString()
+            viewModel.clearErrors()
+        }
+
+        collectOnStarted(viewModel.error) {
+            binding.error.apply {
+                text = it?.get(requireContext())
+                isVisible = it != null
+            }
+        }
+
+        binding.signUpButton.setOnClickListener {
+            viewModel.signUpClicked()
         }
     }
 }
